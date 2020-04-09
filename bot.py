a32ed19513c8de63977e58dc5e5d6978b574ca6f
Joon Guillen <joon@modulogeek.com>
2020-04-09 01:54:36 -0700

    Prototype

---
+++ bot.py
@@ -0,0 +1,79 @@
+import os
+import json
+import requests
+import twitch
+
+TWITCH_USER = os.environ.get('TWITCH_USER')
+TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
+SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
+
+tw = twitch.Helix(TWITCH_CLIENT_ID)
+
+# Get followings
+#followings = tw.user(TWITCH_USER).following().users
+#
+#for user in followings:
+#    print(user)
+#    #if user.is_live:
+#    #    print(f"{user} stream: {user.stream}")
+
+
+user = tw.user(TWITCH_USER)
+stream = user.stream
+
+slack = requests.Session()
+headers = {'Content-Type': 'application/json'}
+
+title = f"*{user.login}* just started a live stream!"
+text = (
+    f"*<https://twitch.tv/{user.login}|{user.display_name} - Twitch>*\n"
+    f"{stream.title}"
+)
+stream_thumbnail_url = stream.thumbnail_url.format(width=800, height=450)
+
+msg = {
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": title
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": text
+            },
+            "accessory": {
+                "type": "image",
+                "image_url": user.profile_image_url,
+                "alt_text": user.login
+            }
+        },
+        # {
+        #     "type": "divider"
+        # },
+        {
+            "type": "image",
+            "title": {
+                "type": "plain_text",
+                "text": "Screenshot",
+                "emoji": True
+            },
+            "image_url": stream_thumbnail_url,
+            "alt_text": stream.title
+        },
+        {
+            "type": "divider"
+        }
+    ]
+}
+
+resp = slack.post(SLACK_WEBHOOK_URL, headers=headers, data=json.dumps(msg))
+print(resp.status_code)
+print(resp.text)
