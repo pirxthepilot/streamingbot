bf2a74814a4a64242f453c1994b4066573cdb1aa
Joon Guillen <joon@modulogeek.com>
2020-04-11 20:15:33 -0700

    Prepare for lambda

--- bot.py
+++ bot.py
@@ -7,6 +7,15 @@ TWITCH_CLIENT_ID = os.environ.get('TWITCH_CLIENT_ID')
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
 
 
-bot = StreamingBot(TWITCH_CLIENT_ID, SLACK_WEBHOOK_URL)
-bot.set_users_to_watch(bot.get_user_follows(TWITCH_USER))
-bot.run()
+def main():
+    bot = StreamingBot(TWITCH_CLIENT_ID, SLACK_WEBHOOK_URL)
+    bot.set_users_to_watch(bot.get_user_follows(TWITCH_USER))
+    bot.run()
+
+
+def lambda_handler():
+    main()
+
+
+if __name__ == '__main__':
+    main()
